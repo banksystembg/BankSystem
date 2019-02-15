2cbcfa97cdceaecc87a946b93d1d9cf7e43e8b9e
Melik Pehlivanov <melikpehlivanov@gmail.com>
2019-02-15 20:42:13 +0200

    Implement listing of user accounts on Index page for logged in users

--- src/Web/BankSystem.Web/Controllers/HomeController.cs
+++ src/Web/BankSystem.Web/Controllers/HomeController.cs
@@ -1,29 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using System.Diagnostics;
 using BankSystem.Web.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BankSystem.Web.Controllers
 {
+    using System.Linq;
+    using System.Threading.Tasks;
+    using AutoMapper;
+    using Models.BankAccount;
+    using Services.Interfaces;
+    using Services.Models.BankAccount;
+
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly IBankAccountService bankAccountService;
+        private readonly IUserService userService;
+
+        public HomeController(IBankAccountService bankAccountService, IUserService userService)
         {
-            return View();
+            this.bankAccountService = bankAccountService;
+            this.userService = userService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var userId = await this.userService.GetUserIdAsyncByUsername(this.User.Identity.Name);
+            var bankAccounts = (await this.bankAccountService.GetAllUserAccountsAsync<BankAccountIndexServiceModel>(userId))
+                .Select(Mapper.Map<BankAccountIndexViewModel>)
+                .ToList();
+
+            var viewModel = new HomeViewModel
+            {
+                UserBankAccounts = bankAccounts,
+            };
+            return this.View(viewModel);
         }
 
         public IActionResult Privacy()
         {
-            return View();
+            return this.View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return this.View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
