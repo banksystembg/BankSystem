f485d7e33559264ea80315ca916ee59bb9aa9bd8
Melik Pehlivanov <melikpehlivanov@gmail.com>
2019-02-15 19:09:19 +0200

    Add service models for BankAccount

---
+++ src/Services/BankSystem.Services.Models/BankAccount/BankAccountIndexServiceModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BankSystem.Services.Models.BankAccount
+{
+    public class BankAccountIndexServiceModel : BankAccountBaseServiceModel
+    {
+        public decimal Balance { get; set; }
+
+        public string UniqueId { get; set; }
+    }
+}
