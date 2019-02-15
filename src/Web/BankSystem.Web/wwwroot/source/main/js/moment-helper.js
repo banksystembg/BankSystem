6cbc12b360b79932877ee0ef473bb06368d7a09e
Martin Mladenov <30376060+martinmladenov@users.noreply.github.com>
2019-02-15 15:51:51 +0200

    Add moment.js + helper

---
+++ src/Web/BankSystem.Web/wwwroot/source/main/js/moment-helper.js
@@ -0,0 +1,23 @@
+function getFriendlyDate(momentTime) {
+    return momentTime.calendar(null,
+        {
+            sameDay: '[Today at] HH:mm',
+            nextDay: '[Tomorrow at] HH:mm',
+            nextWeek: 'dddd [at] HH:mm',
+            lastDay: '[Yesterday at] HH:mm',
+            lastWeek: '[Last] dddd [at] HH:mm',
+            sameElse: 'Do MMM YYYY [at] HH:mm'
+        });
+}
+
+function getFormattedDate(momentTime) {
+    return momentTime.format('DD/MM/YYYY HH:mm');
+}
+
+$('.auto-friendly-date').each(function () {
+    this.innerHTML = getFriendlyDate(moment.utc(this.innerHTML).local());
+});
+
+$('.auto-format-date').each(function () {
+    this.innerHTML = getFormattedDate(moment.utc(this.innerHTML).local());
+});
