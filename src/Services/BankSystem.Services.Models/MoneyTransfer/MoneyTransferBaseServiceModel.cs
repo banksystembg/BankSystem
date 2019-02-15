3ea7189753b4cdb73635162bcfb2a74468910afc
Melik Pehlivanov <melikpehlivanov@gmail.com>
2019-02-15 21:05:50 +0200

    Add models for MoneyTransfers

---
+++ src/Services/BankSystem.Services.Models/MoneyTransfer/MoneyTransferBaseServiceModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BankSystem.Services.Models.MoneyTransfer
+{
+    using BankSystem.Models;
+    using Common.AutoMapping.Interfaces;
+
+    public class MoneyTransferBaseServiceModel : IMapWith<MoneyTransfer>
+    {
+        public string Id { get; set; }
+    }
+}
