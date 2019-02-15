3c75c7ecc398a6f7c885e6b281443efc682626c3
Melik Pehlivanov <melikpehlivanov@gmail.com>
2019-02-15 19:18:31 +0200

    Implement GetUserIdByUsernameAsync method in UserService.cs

---
+++ src/Services/BankSystem.Services/Implementations/UserService.cs
@@ -0,0 +1,24 @@
+﻿namespace BankSystem.Services.Implementations
+{
+    using System.Threading.Tasks;
+    using Data;
+    using Interfaces;
+    using Microsoft.EntityFrameworkCore;
+
+    public class UserService : BaseService, IUserService
+    {
+        public UserService(BankSystemDbContext context) 
+            : base(context)
+        {
+        }
+
+        public async Task<string> GetUserIdAsyncByUsername(string username)
+        {
+            var user = await this.Context
+                .Users
+                .SingleOrDefaultAsync(u => u.UserName == username);
+
+            return user?.Id;
+        }
+    }
+}
