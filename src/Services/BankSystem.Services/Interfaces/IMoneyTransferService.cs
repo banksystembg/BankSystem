177f7ceedd4c7b9c31eb24f97aa33cc68c3bf0dc
Melik Pehlivanov <melikpehlivanov@gmail.com>
2019-02-15 21:06:10 +0200

    Implement GetAllMoneyTransfersForGivenUserByUserIdAsync method in MoneyTransferService.cs

---
+++ src/Services/BankSystem.Services/Interfaces/IMoneyTransferService.cs
@@ -0,0 +1,12 @@
+﻿namespace BankSystem.Services.Interfaces
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Models.MoneyTransfer;
+
+    public interface IMoneyTransferService
+    {
+        Task<IEnumerable<T>> GetAllMoneyTransfersForGivenUserByUserIdAsync<T>(string userId)
+            where T : MoneyTransferBaseServiceModel;
+    }
+}
