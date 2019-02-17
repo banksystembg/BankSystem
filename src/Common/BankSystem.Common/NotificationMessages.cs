d152ae3d39e1af258de49ab0e5e4088db7320ad6
Melik Pehlivanov <melikpehlivanov@gmail.com>
2019-02-17 17:57:54 +0200

    Add new notification messages

--- src/Common/BankSystem.Common/NotificationMessages.cs
+++ src/Common/BankSystem.Common/NotificationMessages.cs
@@ -4,5 +4,9 @@ namespace BankSystem.Common
     {
         public const string BankAccountCreated = "Bank account created successfully";
         public const string BankAccountCreateError = "An error occured while creating bank account";
+        public const string TryAgainLaterError = "Oops! Something went wrong! Please try again later.";
+
+        public const string NoAccountsError =
+            "Looks like you don't have any accounts. Please feel free to create one and then come back again to process your payment.";
     }
 }
