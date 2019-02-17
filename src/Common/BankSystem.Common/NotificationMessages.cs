46d8724e1ec25596542339c46d627502e7b5747b
Martin Mladenov <30376060+martinmladenov@users.noreply.github.com>
2019-02-17 12:07:17 +0200

    Implement bank account creation

--- src/Common/BankSystem.Common/NotificationMessages.cs
+++ src/Common/BankSystem.Common/NotificationMessages.cs
@@ -2,5 +2,7 @@ namespace BankSystem.Common
 {
     public static class NotificationMessages
     {
+        public const string BankAccountCreated = "Bank account created successfully";
+        public const string BankAccountCreateError = "An error occured while creating bank account";
     }
 }
--- src/Web/BankSystem.Web/Controllers/BankAccountsController.cs
+++ src/Web/BankSystem.Web/Controllers/BankAccountsController.cs
@@ -1,14 +1,59 @@
 namespace BankSystem.Web.Controllers
 {
+    using System;
+    using System.Threading.Tasks;
+    using AutoMapper;
+    using Common;
     using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
+    using Models.BankAccount;
+    using Services.Interfaces;
+    using Services.Models.BankAccount;
 
+    [Authorize]
     public class BankAccountsController : BaseController
     {
-        [Authorize]
+        private readonly IBankAccountService bankAccountService;
+        private readonly IUserService userService;
+
+        public BankAccountsController(IBankAccountService bankAccountService, IUserService userService)
+        {
+            this.bankAccountService = bankAccountService;
+            this.userService = userService;
+        }
+
         public IActionResult Create()
         {
             return this.View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(BankAccountCreateBindingModel model)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.View(model);
+            }
+
+            var serviceModel = Mapper.Map<BankAccountCreateServiceModel>(model);
+
+            serviceModel.UserId = await this.userService.GetUserIdByUsernameAsync(this.User.Identity.Name);
+            
+            serviceModel.CreatedOn = DateTime.UtcNow;
+
+            var accountId = await this.bankAccountService.CreateAsync(serviceModel);
+
+            if (accountId == null)
+            {
+                this.Error(NotificationMessages.BankAccountCreateError);
+
+                return this.View(model);
+            }
+
+            this.Success(NotificationMessages.BankAccountCreated);
+
+            // TODO Redirect to account details page
+            return this.RedirectToAction("Index", "Home");
         }
     }
 }
