44355be7fab66d75f01e35e73c525ae2a4654d37
Melik Pehlivanov <melikpehlivanov@gmail.com>
2019-02-17 17:01:58 +0200

    Add MoneyTransfersController.cs

---
+++ src/Web/BankSystem.Web/Controllers/MoneyTransfersController.cs
@@ -0,0 +1,11 @@
+﻿namespace BankSystem.Web.Controllers
+{
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Authorize]
+    public class MoneyTransfersController : BaseController
+    {
+    }
+}
