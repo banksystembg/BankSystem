7a862ac308e26b93f7c36d48c0c9f1c560f25dc8
Melik Pehlivanov <melikpehlivanov@gmail.com>
2019-02-17 18:08:21 +0200

    Add models for moneyTransferCreation

---
+++ src/Services/BankSystem.Services.Models/MoneyTransfer/MoneyTransferCreateServiceModel.cs
@@ -0,0 +1,27 @@
+﻿namespace BankSystem.Services.Models.MoneyTransfer
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class MoneyTransferCreateServiceModel : MoneyTransferBaseServiceModel
+    {
+        [Required]
+        public string Details { get; set; }
+
+        [Required]
+        [Range(typeof(decimal), "0.01", "79228162514264337593543950335")]
+        public decimal Amount { get; set; }
+
+        [Required]
+        public string AccountId { get; set; }
+
+        [Required]
+        public DateTime MadeOn { get; set; } = DateTime.UtcNow;
+
+        [Required]
+        public string Source => this.AccountId;
+
+        [Required]
+        public string Destination { get; set; }
+    }
+}
---
+++ src/Web/BankSystem.Web/Models/MoneyTransfer/MoneyTransferCreateBindingModel.cs
@@ -0,0 +1,29 @@
+﻿namespace BankSystem.Web.Models.MoneyTransfer
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using Common.AutoMapping.Interfaces;
+    using Microsoft.AspNetCore.Mvc.Rendering;
+    using Services.Models.MoneyTransfer;
+
+    public class MoneyTransferCreateBindingModel : IMapWith<MoneyTransferCreateServiceModel>
+    {
+        [Required]
+        [Display(Name = "Destination account")]
+        public string Destination { get; set; }
+
+        [Required]
+        [Display(Name = "Details of Payment")]
+        public string Details { get; set; }
+
+        [Required]
+        [Range(typeof(decimal), "0.01", "79228162514264337593543950335")]
+        public decimal Amount { get; set; }
+
+        [Required]
+        [Display(Name = "From Account")]
+        public string AccountId { get; set; }
+        
+        public IEnumerable<SelectListItem> UserAccounts { get; set; }
+    }
+}
