bbb5f4484a398943f4d0e0e18e2f63910a20e2cc
Melik Pehlivanov <melikpehlivanov@gmail.com>
2019-02-17 18:27:19 +0200

    Add money transfer create page

--- src/Web/BankSystem.Web/Controllers/MoneyTransfersController.cs
+++ src/Web/BankSystem.Web/Controllers/MoneyTransfersController.cs
@@ -1,11 +1,57 @@
 ﻿namespace BankSystem.Web.Controllers
 {
+    using System.Collections.Generic;
+    using System.Linq;
     using System.Threading.Tasks;
+    using Common;
     using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Rendering;
+    using Models.MoneyTransfer;
+    using Services.Interfaces;
+    using Services.Models.BankAccount;
 
     [Authorize]
     public class MoneyTransfersController : BaseController
     {
+        private readonly IBankAccountService bankAccountService;
+        private readonly IUserService userService;
+
+        public MoneyTransfersController(IBankAccountService bankAccountService, IUserService userService)
+        {
+            this.bankAccountService = bankAccountService;
+            this.userService = userService;
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            var userId = await this.userService.GetUserIdByUsernameAsync(this.User.Identity.Name);
+            if (userId == null)
+            {
+                this.ShowErrorMessage(NotificationMessages.TryAgainLaterError);
+                return this.RedirectToAction("Index", "Home");
+            }
+            var userAccounts = await this.GetAllUserAccountsAsync(userId);
+            if (!userAccounts.Any())
+            {
+                this.ShowErrorMessage(NotificationMessages.NoAccountsError);
+                return this.RedirectToAction("Index", "Home");
+            }
+            var model = new MoneyTransferCreateBindingModel
+            {
+                UserAccounts = userAccounts,
+            };
+            return this.View(model);
+        }
+
+        private async Task<IEnumerable<SelectListItem>> GetAllUserAccountsAsync(string userId)
+        {
+            var userAccounts = await this.bankAccountService
+                .GetAllUserAccountsAsync<BankAccountIndexServiceModel>(userId);
+
+            return userAccounts
+                .Select(a => new SelectListItem { Text = a.Name, Value = a.Id })
+                .ToArray();
+        }
     }
 }
