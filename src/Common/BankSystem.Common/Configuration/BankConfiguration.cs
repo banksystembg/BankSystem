f25629e942857cf3b53eb756bdbdde815ba13776
Melik Pehlivanov <melikpehlivanov@gmail.com>
2019-02-18 19:24:34 +0200

    Add auto-generated AppId and ApiKey from CentralApi for BankSystem.Web

--- src/Common/BankSystem.Common/Configuration/BankConfiguration.cs
+++ src/Common/BankSystem.Common/Configuration/BankConfiguration.cs
@@ -7,5 +7,11 @@ namespace BankSystem.Common.Configuration
         [Required]
         [RegularExpression(@"^[A-Z]{3}$")]
         public string UniqueIdentifier { get; set; }
+
+        [Required]
+        public string AppId { get; set; }
+
+        [Required]
+        public string ApiKey { get; set; }
     }
 }
