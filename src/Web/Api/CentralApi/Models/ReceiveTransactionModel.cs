7429b839d135617a191302104ed1fd6716083287
Melik Pehlivanov <melikpehlivanov@gmail.com>
2019-02-18 20:19:44 +0200

    Add ReceiveTransactionModel model in CentralApi

---
+++ src/Web/Api/CentralApi/Models/ReceiveTransactionModel.cs
@@ -0,0 +1,30 @@
+﻿namespace CentralApi.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class ReceiveTransactionModel
+    {
+        // Which account sends the money
+        [Required]
+        public string AccountId { get; set; }
+
+        // To which account the money should be send
+        [Required]
+        public string Destination { get; set; }
+
+        [Required]
+        public string DestinationBankSwiftCode { get; set; }
+
+        [Required]
+        public string DestinationBankName { get; set; }
+
+        [Required]
+        public string DestinationBankCountry { get; set; }
+
+        public string Description { get; set; }
+
+        [Required]
+        public decimal Amount { get; set; }
+
+    }
+}
