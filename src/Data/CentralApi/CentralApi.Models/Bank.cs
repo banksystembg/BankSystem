c0dbbdc1605b307612947819c413e8346f9e5444
Melik Pehlivanov <melikpehlivanov@gmail.com>
2019-02-18 18:43:34 +0200

    Add Bank model for CentralApi

---
+++ src/Data/CentralApi/CentralApi.Models/Bank.cs
@@ -0,0 +1,21 @@
+﻿namespace CentralApi.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Bank
+    {
+        public string Id { get; set; }
+
+        [Required]
+        public string ShortName { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string UniqueIdentifier { get; set; }
+
+        [Required]
+        public string Location { get; set; }
+    }
+}
