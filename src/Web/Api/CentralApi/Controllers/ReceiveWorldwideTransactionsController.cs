eb68f1580aa448dfacbd874e3f220c4294fe9251
Melik Pehlivanov <melikpehlivanov@gmail.com>
2019-02-19 19:31:35 +0200

    Rename ValuesController.cs to ReceiveWorldwideTransactionsController.cs

---
+++ src/Web/Api/CentralApi/Controllers/ReceiveWorldwideTransactionsController.cs
@@ -0,0 +1,18 @@
+﻿namespace CentralApi.Controllers
+{
+    using Infrastructure.Filters;
+    using Microsoft.AspNetCore.Mvc;
+    using Models;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnsureRequestIsValid]
+    public class ReceiveWorldwideTransactionsController : ControllerBase
+    {
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] ReceiveTransactionModel model)
+        {
+        }
+    }
+}
