7e08c52872c6061565c61b2cb2d86599c643d09f
Melik Pehlivanov <melikpehlivanov@gmail.com>
2019-02-19 19:31:19 +0200

    Update NotificationMessages.cs

--- src/Common/BankSystem.Common/NotificationMessages.cs
+++ src/Common/BankSystem.Common/NotificationMessages.cs
@@ -4,7 +4,8 @@ namespace BankSystem.Common
     {
         public const string BankAccountCreated = "Bank account created successfully";
         public const string BankAccountCreateError = "An error occured while creating bank account";
-        public const string TryAgainLaterError = "Oops! Something went wrong! Please try again later.";
+        public const string TryAgainLaterError =
+            "Oops! Something went wrong! Please try again later. If this error continues to occur, please contact our support center";
 
         public const string NoAccountsError =
             "Looks like you don't have any accounts. Please feel free to create one and then come back again to process your payment.";
