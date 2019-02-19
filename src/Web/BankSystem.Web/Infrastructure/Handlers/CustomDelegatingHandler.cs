0644103e078c019688657ed472233c6575b7514d
Melik Pehlivanov <melikpehlivanov@gmail.com>
2019-02-19 19:25:09 +0200

    Refactor CustomDelegatingHandler.cs

--- src/Web/BankSystem.Web/Infrastructure/Handlers/CustomDelegatingHandler.cs
+++ src/Web/BankSystem.Web/Infrastructure/Handlers/CustomDelegatingHandler.cs
@@ -7,6 +7,7 @@
     using System.Text;
     using System.Threading;
     using System.Threading.Tasks;
+    using System.Web;
 
     public class CustomDelegatingHandler : DelegatingHandler
     {
@@ -21,34 +22,14 @@
 
         protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
-            var requestContentBase64String = string.Empty;
-            var requestUri = System.Web.HttpUtility.UrlEncode(request.RequestUri.AbsoluteUri.ToLower());
-            var requestHttpMethod = request.Method.Method;
-
-            //Calculate UNIX time
-            var epochStart = new DateTime(1970, 01, 01, 0, 0, 0, 0, DateTimeKind.Utc);
-            var timeSpan = DateTime.UtcNow - epochStart;
-            var requestTimeStamp = Convert.ToUInt64(timeSpan.TotalSeconds).ToString();
-
-            //create random nonce for each request
-            var nonce = Guid.NewGuid().ToString("N");
-
-            if (request.Content != null)
-            {
-                var content = await request.Content.ReadAsByteArrayAsync();
-                var sha256 = SHA256.Create();
-
-                //Hashing the request body
-                var requestContentHash = sha256.ComputeHash(content);
-                requestContentBase64String = Convert.ToBase64String(requestContentHash);
-            }
+            var requestUri = HttpUtility.UrlEncode(request.RequestUri.AbsoluteUri.ToLower());
+            var requestHttpMethod = request.Method;
 
             //Raw signature string
             var signatureRawData =
-                $"{this.appId}{requestHttpMethod}{requestUri}{requestTimeStamp}{nonce}{requestContentBase64String}";
+                $"{this.appId}{requestHttpMethod}{requestUri}";
 
             var secretKeyByteArray = Convert.FromBase64String(this.apiKey);
-
             var signature = Encoding.UTF8.GetBytes(signatureRawData);
 
             using (var hmac = new HMACSHA256(secretKeyByteArray))
@@ -58,7 +39,7 @@
 
                 //Setting the values in the Authorization header using custom scheme (bsw)
                 request.Headers.Authorization = new AuthenticationHeaderValue("bsw",
-                    $"{this.appId},{requestSignatureBase64String},{nonce},{requestTimeStamp}");
+                    $"{this.appId},{requestSignatureBase64String}");
             }
 
             var response = await base.SendAsync(request, cancellationToken);
