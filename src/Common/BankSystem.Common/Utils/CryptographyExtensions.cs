74f7c612a7065372d0128b8370eaf2962efea7d3
Melik Pehlivanov <melikpehlivanov@gmail.com>
2019-02-21 23:17:38 +0200

    Add CryptographyExtensions.cs

---
+++ src/Common/BankSystem.Common/Utils/CryptographyExtensions.cs
@@ -0,0 +1,70 @@
+﻿namespace BankSystem.Common.Utils
+{
+    using System;
+    using System.IO;
+    using System.Security.Cryptography;
+
+    public static class CryptographyExtensions
+    {
+        public static string[] GenerateKey()
+        {
+            using (AesManaged aes = new AesManaged())
+            {
+                aes.GenerateKey();
+                aes.GenerateIV();
+
+                return new[] { Convert.ToBase64String(aes.Key), Convert.ToBase64String(aes.IV) };
+            }
+
+        }
+
+        public static byte[] Encrypt(string plainText, byte[] Key, byte[] IV)
+        {
+            byte[] encrypted;
+            // Create a new AesManaged.    
+            using (AesManaged aes = new AesManaged())
+            {
+                // Create encryptor    
+                ICryptoTransform encryptor = aes.CreateEncryptor(Key, IV);
+                // Create MemoryStream    
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    // Create crypto stream using the CryptoStream class. This class is the key to encryption    
+                    // and encrypts and decrypts data from any given stream. In this case, we will pass a memory stream    
+                    // to encrypt    
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                    {
+                        // Create StreamWriter and write data to a stream    
+                        using (StreamWriter sw = new StreamWriter(cs))
+                            sw.Write(plainText);
+                        encrypted = ms.ToArray();
+                    }
+                }
+            }
+            // Return encrypted data    
+            return encrypted;
+        }
+        public static string Decrypt(byte[] cipherText, byte[] Key, byte[] IV)
+        {
+            string plaintext = null;
+            // Create AesManaged    
+            using (AesManaged aes = new AesManaged())
+            {
+                // Create a decryptor    
+                ICryptoTransform decryptor = aes.CreateDecryptor(Key, IV);
+                // Create the streams used for decryption.    
+                using (MemoryStream ms = new MemoryStream(cipherText))
+                {
+                    // Create crypto stream    
+                    using (CryptoStream cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
+                    {
+                        // Read crypto stream    
+                        using (StreamReader reader = new StreamReader(cs))
+                            plaintext = reader.ReadToEnd();
+                    }
+                }
+            }
+            return plaintext;
+        }
+    }
+}
