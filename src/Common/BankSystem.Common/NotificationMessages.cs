d26da1e6e8274474e9d09826022c1741271a04a7
Melik Pehlivanov <melikpehlivanov@gmail.com>
2019-02-21 08:58:46 +0200

    Add new notification messages

--- src/Common/BankSystem.Common/NotificationMessages.cs
+++ src/Common/BankSystem.Common/NotificationMessages.cs
@@ -4,8 +4,11 @@ namespace BankSystem.Common
     {
         public const string BankAccountCreated = "Bank account created successfully";
         public const string BankAccountCreateError = "An error occured while creating bank account";
-        public const string TryAgainLaterError =
+        public const string TryAgainLaterError = 
             "Oops! Something went wrong! Please try again later. If this error continues to occur, please contact our support center";
+
+        public const string InsufficientFunds = 
+            "insufficient account balance. Please deposit money in your account and come back to process the payment or choose another account";
 
         public const string NoAccountsError =
             "Looks like you don't have any accounts. Please feel free to create one and then come back again to process your payment.";
