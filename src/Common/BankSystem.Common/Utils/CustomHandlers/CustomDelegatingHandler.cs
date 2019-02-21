117951572d7711cb40c673a74be10774dd44d737
Melik Pehlivanov <melikpehlivanov@gmail.com>
2019-02-21 08:57:21 +0200

    Encrypt data before it's send to the api

--- src/Common/BankSystem.Common/Utils/CustomHandlers/CustomDelegatingHandler.cs
+++ src/Common/BankSystem.Common/Utils/CustomHandlers/CustomDelegatingHandler.cs
@@ -4,36 +4,51 @@
     using System.Net.Http;
     using System.Net.Http.Headers;
     using System.Security.Cryptography;
-    using System.Text;
     using System.Threading;
     using System.Threading.Tasks;
 
     public class CustomDelegatingHandler : DelegatingHandler
     {
-        private readonly string key;
+        private readonly string apiSigningKey;
+        private readonly string bankKey;
         private readonly string bankName;
         private readonly string bankSwiftCode;
 
-        public CustomDelegatingHandler(string key, string bankName, string bankSwiftCode)
+        public CustomDelegatingHandler(string apiSigningKey, string bankKey)
         {
-            this.key = key;
+            this.apiSigningKey = apiSigningKey;
+            this.bankKey = bankKey;
+        }
+
+        public CustomDelegatingHandler(string apiSigningKey, string bankKey, string bankName, string bankSwiftCode)
+        {
+            this.apiSigningKey = apiSigningKey;
+            this.bankKey = bankKey;
             this.bankName = bankName;
             this.bankSwiftCode = bankSwiftCode;
         }
 
+
         protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
-            var rsa = RSA.Create();
-            RsaExtensions.FromXmlString(rsa, this.key);
+            // Sign data with bank private key
+            string requestSignedData;
+            using (var rsa = RSA.Create())
+            {
+                RsaExtensions.FromXmlString(rsa, this.bankKey);
 
-            // TODO: Encrypt data
+                var content = await request.Content.ReadAsByteArrayAsync();
+                var signedData = rsa.SignData(content, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
 
-            var content = await request.Content.ReadAsByteArrayAsync();
-            var requestContentBase64String = Convert.ToBase64String(content);
+                // Encrypt data with central api public key
+                // We might use AES because the data might exceed the limit (490-500 bytes) and it will not be able to encrypt correctly
+                using (var encryptionRsa = RSA.Create())
+                {
+                    RsaExtensions.FromXmlString(encryptionRsa, this.apiSigningKey);
+                    requestSignedData = Convert.ToBase64String(encryptionRsa.Encrypt(signedData, RSAEncryptionPadding.Pkcs1));
+                }
+            }
 
-            var signature = Encoding.UTF8.GetBytes(requestContentBase64String);
-            var signedData = rsa.SignData(signature, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
-            var requestSignedData = Convert.ToBase64String(signedData);
             //Setting the values in the Authorization header using custom scheme (bsw)
             request.Headers.Authorization = new AuthenticationHeaderValue("bsw",
                 $"{this.bankName},{this.bankSwiftCode},{requestSignedData}");
