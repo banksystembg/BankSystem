e0bb12c164f1d64e4e5f18e112dd390830c9438b
Martin Mladenov <30376060+martinmladenov@users.noreply.github.com>
2019-02-24 17:57:26 +0200

    Implement account settings index page

---
+++ src/Web/BankSystem.Web/Pages/Account/Settings/Index.cshtml.cs
@@ -0,0 +1,39 @@
+﻿namespace BankSystem.Web.Pages.Account.Settings
+{
+    using System.Threading.Tasks;
+    using BankSystem.Models;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+
+    [Authorize]
+    public class IndexModel : BasePageModel
+    {
+        private readonly UserManager<BankUser> userManager;
+
+        public IndexModel(
+            UserManager<BankUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        public string Email { get; set; }
+
+        public string FullName { get; set; }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var user = await this.userManager.GetUserAsync(this.User);
+            if (user == null)
+            {
+                return this.Forbid();
+            }
+
+            this.FullName = user.FullName;
+            this.Email = user.Email;
+
+            return this.Page();
+        }
+    }
+}
