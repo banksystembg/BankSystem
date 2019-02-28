0752dc779a1c41bd8830219c684bdc51891294a3
Melik Pehlivanov <melikpehlivanov@gmail.com>
2019-02-28 19:40:57 +0200

    Implement bank account details

--- src/Web/BankSystem.Web/Controllers/BankAccountsController.cs
+++ src/Web/BankSystem.Web/Controllers/BankAccountsController.cs
@@ -1,22 +1,27 @@
 namespace BankSystem.Web.Controllers
 {
+    using System.Linq;
     using System.Threading.Tasks;
+    using Areas.MoneyTransfers.Models;
     using AutoMapper;
     using Common;
     using Microsoft.AspNetCore.Mvc;
     using Models.BankAccount;
     using Services.Interfaces;
     using Services.Models.BankAccount;
+    using Services.Models.MoneyTransfer;
 
     public class BankAccountsController : BaseController
     {
         private readonly IBankAccountService bankAccountService;
         private readonly IUserService userService;
+        private readonly IMoneyTransferService moneyTransferService;
 
-        public BankAccountsController(IBankAccountService bankAccountService, IUserService userService)
+        public BankAccountsController(IBankAccountService bankAccountService, IUserService userService, IMoneyTransferService moneyTransferService)
         {
             this.bankAccountService = bankAccountService;
             this.userService = userService;
+            this.moneyTransferService = moneyTransferService;
         }
 
         public IActionResult Create()
@@ -35,7 +40,7 @@ namespace BankSystem.Web.Controllers
             var serviceModel = Mapper.Map<BankAccountCreateServiceModel>(model);
 
             serviceModel.UserId = await this.userService.GetUserIdByUsernameAsync(this.User.Identity.Name);
-            
+
             var accountId = await this.bankAccountService.CreateAsync(serviceModel);
 
             if (accountId == null)
@@ -49,6 +54,16 @@ namespace BankSystem.Web.Controllers
 
             // TODO Redirect to account details page
             return this.RedirectToAction("Index", "Home");
+        }
+
+        public async Task<IActionResult> Details(string id)
+        {
+            var transfers = (await this.moneyTransferService
+                    .GetAllMoneyTransfersForAccountAsync<MoneyTransferListingServiceModel>(id))
+                .Select(Mapper.Map<MoneyTransferListingViewModel>)
+                .ToArray();
+
+            return this.View(transfers);
         }
     }
 }
