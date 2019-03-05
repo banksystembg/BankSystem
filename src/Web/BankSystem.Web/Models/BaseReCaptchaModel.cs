afa1e9f274d1c2d2ed59f68f0d96bc983ff188fb
Melik Pehlivanov <melikpehlivanov@gmail.com>
2019-03-05 22:02:05 +0200

    Add base model for ReCaptcha

---
+++ src/Web/BankSystem.Web/Models/BaseReCaptchaModel.cs
@@ -0,0 +1,14 @@
+﻿namespace BankSystem.Web.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using Infrastructure.Attributes;
+    using Microsoft.AspNetCore.Mvc;
+
+    public abstract class BaseReCaptchaModel
+    {
+        [Required]
+        [ValidateReCaptcha]
+        [BindProperty(Name = "g-recaptcha-response")]
+        public string ReCaptchaResponse { get; set; }
+    }
+}
