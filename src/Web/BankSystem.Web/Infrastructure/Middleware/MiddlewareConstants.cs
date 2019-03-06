5f46b8da806a0c4cb1adb82844afbb967691702b
Melik Pehlivanov <melikpehlivanov@gmail.com>
2019-03-06 20:31:01 +0200

    Add SecurityHeadersBuilder and MiddlewareConstants

---
+++ src/Web/BankSystem.Web/Infrastructure/Middleware/MiddlewareConstants.cs
@@ -0,0 +1,45 @@
+﻿namespace BankSystem.Web.Infrastructure.Middleware
+{
+    public class MiddlewareConstants
+    {
+        public class ContentTypeOptions
+        {
+            public const string Header = "X-Content-Type-Options";
+            public const string NoSniff = "nosniff";
+        }
+
+        public class FeaturePolicy
+        {
+            public const string Header = "Feature-Policy";
+            public const string Ignore = "microphone 'none'; geolocation 'none'; camera 'none'";
+        }
+
+        public class ReferrerPolicy
+        {
+            public const string Header = "Referrer-Policy";
+            public const string NoReferrer = "no-referrer";
+        }
+
+        public class FrameOptions
+        {
+            public const string Header = "X-Frame-Options";
+            public const string Deny = "DENY";
+            public const string SameOrigin = "SAMEORIGIN";
+            public const string AllowFromUri = "ALLOW-FROM {0}";
+        }
+
+        public class Server
+        {
+            public const string Header = "Server";
+        }
+
+        public class XssProtection
+        {
+            public const string Header = "X-XSS-Protection";
+            public const string Enabled = "1";
+            public const string Disabled = "0";
+            public const string Block = "1; mode=block";
+            public const string Report = "1; report={0}";
+        }
+    }
+}
---
+++ src/Web/BankSystem.Web/Infrastructure/Middleware/SecurityHeadersBuilder.cs
@@ -0,0 +1,59 @@
+﻿namespace BankSystem.Web.Infrastructure.Middleware
+{
+    using System;
+
+    public class SecurityHeadersBuilder
+    {
+        private readonly SecurityHeadersPolicy policy = new SecurityHeadersPolicy();
+
+        public SecurityHeadersBuilder AddDefaultSecurePolicy()
+        {
+            this.AddFrameOptionsSameOrigin();
+            this.AddFeature();
+            this.AddReferrer();
+            this.AddXssProtection();
+            this.AddContentTypeOptionsNoSniff();
+
+            return this;
+        }
+
+        public void AddFrameOptionsSameOrigin()
+            => this.AddCustomHeader(MiddlewareConstants.FrameOptions.Header, MiddlewareConstants.FrameOptions.SameOrigin);
+
+        public void AddFeature()
+           => this.AddCustomHeader(MiddlewareConstants.FeaturePolicy.Header, MiddlewareConstants.FeaturePolicy.Ignore);
+
+        public void AddReferrer()
+           => this.AddCustomHeader(MiddlewareConstants.ReferrerPolicy.Header, MiddlewareConstants.ReferrerPolicy.NoReferrer);
+
+        public void AddXssProtection()
+           => this.AddCustomHeader(MiddlewareConstants.XssProtection.Header, MiddlewareConstants.XssProtection.Block);
+
+        public void AddContentTypeOptionsNoSniff()
+          => this.AddCustomHeader(MiddlewareConstants.ContentTypeOptions.Header, MiddlewareConstants.ContentTypeOptions.NoSniff);
+
+        public SecurityHeadersBuilder AddCustomHeader(string header, string value)
+        {
+            if (string.IsNullOrEmpty(header))
+            {
+                throw new ArgumentNullException(nameof(header));
+            }
+
+            this.policy.HeadersToSet[header] = value;
+            return this;
+        }
+
+        public SecurityHeadersBuilder RemoveHeader(string header)
+        {
+            if (string.IsNullOrEmpty(header))
+            {
+                throw new ArgumentNullException(nameof(header));
+            }
+
+            this.policy.HeadersToRemove.Add(header);
+            return this;
+        }
+
+        public SecurityHeadersPolicy Policy() => this.policy;
+    }
+}
