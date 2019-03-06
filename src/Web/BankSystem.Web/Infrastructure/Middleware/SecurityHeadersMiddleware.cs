5e29450da1fbe4cb714a5d270660f1c62097d7c9
Melik Pehlivanov <melikpehlivanov@gmail.com>
2019-03-06 20:30:41 +0200

    Add SecurityHeadersMiddleware

---
+++ src/Web/BankSystem.Web/Infrastructure/Middleware/SecurityHeadersMiddleware.cs
@@ -0,0 +1,34 @@
+﻿namespace BankSystem.Web.Infrastructure.Middleware
+{
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Http;
+
+    public class SecurityHeadersMiddleware
+    {
+        private readonly RequestDelegate next;
+        private readonly SecurityHeadersPolicy policy;
+
+        public SecurityHeadersMiddleware(RequestDelegate next, SecurityHeadersPolicy policy)
+        {
+            this.next = next;
+            this.policy = policy;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var headers = context.Response.Headers;
+
+            foreach (var (key, value) in this.policy.HeadersToSet)
+            {
+                headers[key] = value;
+            }
+
+            foreach (var header in this.policy.HeadersToRemove)
+            {
+                headers.Remove(header);
+            }
+
+            await this.next(context);
+        }
+    }
+}
---
+++ src/Web/BankSystem.Web/Infrastructure/Middleware/SecurityHeadersPolicy.cs
@@ -0,0 +1,11 @@
+﻿namespace BankSystem.Web.Infrastructure.Middleware
+{
+    using System.Collections.Generic;
+
+    public class SecurityHeadersPolicy
+    {
+        public IDictionary<string, string> HeadersToSet { get; } = new Dictionary<string, string>();
+
+        public ISet<string> HeadersToRemove { get; } = new HashSet<string>();
+    }
+}
