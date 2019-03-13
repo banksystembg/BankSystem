ffe00b228913ba69b7cd5163ee056c6cab3c13dd
Melik Pehlivanov <melikpehlivanov@gmail.com>
2019-03-13 18:13:51 +0200

    Add card create view

---
+++ src/Web/BankSystem.Web/Areas/Cards/Controllers/CardsController.cs
@@ -0,0 +1,43 @@
+﻿namespace BankSystem.Web.Areas.Cards.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Rendering;
+    using Models;
+    using Services.Interfaces;
+    using Services.Models.BankAccount;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class CardsController : BaseCardsController
+    {
+        private readonly IUserService userService;
+        private readonly IBankAccountService bankAccountService;
+
+        public CardsController(IUserService userService, IBankAccountService bankAccountService)
+        {
+            this.userService = userService;
+            this.bankAccountService = bankAccountService;
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            var userId = await this.userService.GetUserIdByUsernameAsync(this.User.Identity.Name);
+            var userAccounts = await this.GetAllAccountsAsync(userId);
+
+            var model = new CardCreateViewModel
+            {
+                BankAccounts = userAccounts,
+            };
+
+            return this.View(model);
+        }
+
+        private async Task<IEnumerable<SelectListItem>> GetAllAccountsAsync(string userId)
+            => (await this.bankAccountService
+                    .GetAllUserAccountsAsync<BankAccountIndexServiceModel>(userId))
+                .Select(a => new SelectListItem { Text = a.Name, Value = a.Id })
+                .ToArray();
+
+    }
+}
---
+++ src/Web/BankSystem.Web/Areas/Cards/Models/CardCreateViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace BankSystem.Web.Areas.Cards.Models
+{
+    using Microsoft.AspNetCore.Mvc.Rendering;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class CardCreateViewModel
+    {
+        public IEnumerable<SelectListItem> BankAccounts { get; set; }
+
+        [Required]
+        [Display(Name = "Choose account")]
+        public string BankAccountId { get; set; }
+    }
+}
