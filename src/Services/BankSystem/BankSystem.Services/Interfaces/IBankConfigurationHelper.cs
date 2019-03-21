0ae15b1ff2d31ab5468cdbd36eac52eb3081a878
Martin Mladenov <30376060+martinmladenov@users.noreply.github.com>
2019-03-21 18:10:37 +0200

    Move BankName and CentralApiAddress to appsettings.json

--- src/Services/BankSystem/BankSystem.Services/Interfaces/IBankConfigurationHelper.cs
+++ src/Services/BankSystem/BankSystem.Services/Interfaces/IBankConfigurationHelper.cs
@@ -6,5 +6,7 @@
         string Key { get; }
         string CentralApiPublicKey { get; }
         string First3CardDigits { get; }
+        string BankName { get; }
+        string CentralApiAddress { get; }
     }
 }
