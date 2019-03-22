68ebeb496d9e542a58b6a93e53d6eb8a6837f767
Melik Pehlivanov <melikpehlivanov@gmail.com>
2019-03-22 21:46:41 +0200

    Add method overload in EmailSender.cs

--- src/Common/BankSystem.Common/EmailSender/Implementation/EmailSender.cs
+++ src/Common/BankSystem.Common/EmailSender/Implementation/EmailSender.cs
@@ -9,6 +9,7 @@
 
     public class EmailSender : IEmailSender
     {
+        private const string AppEmail = "banksystem01@gmail.com";
         private readonly SendGridConfiguration options;
 
         public EmailSender(IOptions<SendGridConfiguration> options)
@@ -16,9 +17,20 @@
             this.options = options.Value;
         }
 
+        public async Task<bool> SendEmailAsync(string receiver, string subject, string htmlMessage)
+        {
+            var client = new SendGridClient(this.options.ApiKey);
+            var from = new EmailAddress(AppEmail);
+            var to = new EmailAddress(receiver, receiver);
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, htmlMessage, htmlMessage);
+            var isSuccessful = await client.SendEmailAsync(msg);
+
+            return isSuccessful.StatusCode == HttpStatusCode.Accepted;
+        }
+
         public async Task<bool> SendEmailAsync(string sender, string receiver, string subject, string htmlMessage)
         {
-            var client = new SendGridClient(this.options.SendGridApiKey);
+            var client = new SendGridClient(this.options.ApiKey);
             var from = new EmailAddress(sender);
             var to = new EmailAddress(receiver, receiver);
             var msg = MailHelper.CreateSingleEmail(from, to, subject, htmlMessage, htmlMessage);
--- src/Common/BankSystem.Common/EmailSender/Interface/IEmailSender.cs
+++ src/Common/BankSystem.Common/EmailSender/Interface/IEmailSender.cs
@@ -4,6 +4,7 @@
 
     public interface IEmailSender
     {
+        Task<bool> SendEmailAsync(string receiver, string subject, string htmlMessage);
         Task<bool> SendEmailAsync(string sender, string receiver, string subject, string htmlMessage);
     }
 }
