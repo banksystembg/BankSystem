e0a2d1fd27094169f79a3d33d8b65294d7e9dc8d
Melik Pehlivanov <melikpehlivanov@gmail.com>
2019-03-23 20:49:22 +0200

    Remove unused methods

--- src/Web/BankSystem.Web/Controllers/HomeController.cs
+++ src/Web/BankSystem.Web/Controllers/HomeController.cs
@@ -55,16 +55,5 @@
             };
             return this.View(viewModel);
         }
-
-        public IActionResult Privacy()
-        {
-            return this.View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return this.View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? this.HttpContext.TraceIdentifier });
-        }
     }
 }
