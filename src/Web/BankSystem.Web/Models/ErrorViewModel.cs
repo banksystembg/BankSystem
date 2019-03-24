8e389a01365c18019e5360b814263b1cb804bd0b
Martin Mladenov <30376060+martinmladenov@users.noreply.github.com>
2019-03-24 10:17:05 +0200

    Remove unused model and views

--- src/Web/BankSystem.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BankSystem.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(this.RequestId);
-    }
-}
