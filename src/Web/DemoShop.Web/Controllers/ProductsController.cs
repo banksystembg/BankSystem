89f3efe92c5b041428f87baec653176f972497e7
Martin Mladenov <30376060+martinmladenov@users.noreply.github.com>
2019-03-28 09:40:56 +0200

    Implement product creation

---
+++ src/Web/DemoShop.Web/Controllers/ProductsController.cs
@@ -0,0 +1,44 @@
+namespace DemoShop.Web.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using Models;
+    using Services.Interfaces;
+    using Services.Models;
+
+    [Authorize]
+    public class ProductsController : Controller
+    {
+        private readonly IProductsService productsService;
+
+        public ProductsController(IProductsService productsService)
+        {
+            this.productsService = productsService;
+        }
+
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(ProductCreateBindingModel model)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.View(model);
+            }
+
+            var serviceProduct = new ProductCreateServiceModel
+            {
+                Name = model.Name,
+                Price = model.Price,
+                ImageUrl = model.ImageUrl
+            };
+
+            this.productsService.CreateAsync(serviceProduct);
+
+            return this.RedirectToAction("Index", "Home");
+        }
+    }
+}
