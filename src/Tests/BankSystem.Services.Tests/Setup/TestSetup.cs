838134fd567bcf410894d688140cdb0af85ea8a1
Melik Pehlivanov <melikpehlivanov@gmail.com>
2019-03-28 19:15:37 +0200

    Add initial unit tests project strucute

---
+++ src/Tests/BankSystem.Services.Tests/Setup/TestSetup.cs
@@ -0,0 +1,27 @@
+﻿namespace BankSystem.Services.Tests.Setup
+{
+    using AutoMapper;
+    using Common.AutoMapping.Profiles;
+
+    public class TestSetup
+    {
+        private static readonly object Sync = new object();
+        private static bool mapperInitialized = false;
+
+        public static void InitializeMapper()
+        {
+            lock (Sync)
+            {
+                if (!mapperInitialized)
+                {
+                    Mapper.Initialize(config =>
+                    {
+                        config.AddProfile<DefaultProfile>();
+                    });
+
+                    mapperInitialized = true;
+                }
+            }
+        }
+    }
+}
---
+++ src/Tests/BankSystem.Services.Tests/Tests/BaseTest.cs
@@ -0,0 +1,22 @@
+﻿namespace BankSystem.Services.Tests.Tests
+{
+    using Data;
+    using Microsoft.EntityFrameworkCore;
+    using System;
+
+    public abstract class BaseTest
+    {
+        protected BankSystemDbContext DatabaseInstance
+        {
+            get
+            {
+                var options = new DbContextOptionsBuilder<BankSystemDbContext>()
+                    .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                    .EnableSensitiveDataLogging()
+                    .Options;
+
+                return new BankSystemDbContext(options);
+            }
+        }
+    }
+}
