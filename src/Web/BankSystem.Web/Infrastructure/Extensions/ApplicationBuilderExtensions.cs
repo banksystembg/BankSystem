7215dd68d104cb2f78e9e52dba7db425d9be097f
Melik Pehlivanov <melikpehlivanov@gmail.com>
2019-03-31 18:13:58 +0300

    Seed default user with 10k balance

--- src/Web/BankSystem.Web/Infrastructure/Extensions/ApplicationBuilderExtensions.cs
+++ src/Web/BankSystem.Web/Infrastructure/Extensions/ApplicationBuilderExtensions.cs
@@ -1,6 +1,10 @@
 ﻿namespace BankSystem.Web.Infrastructure.Extensions
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
     using System.Threading.Tasks;
+    using BankSystem.Models;
     using Common;
     using Data;
     using Microsoft.AspNetCore.Builder;
@@ -24,10 +28,11 @@
             using (var serviceScope = app.ApplicationServices.CreateScope())
             {
                 var dbContext = serviceScope.ServiceProvider.GetRequiredService<BankSystemDbContext>();
+                var userManager = serviceScope.ServiceProvider.GetService<UserManager<BankUser>>();
+                var roleManager = serviceScope.ServiceProvider.GetService<RoleManager<IdentityRole>>();
 
                 await dbContext.Database.MigrateAsync();
-
-                var roleManager = serviceScope.ServiceProvider.GetService<RoleManager<IdentityRole>>();
+                await SeedUser(userManager, dbContext);
 
                 if (!await roleManager.RoleExistsAsync(GlobalConstants.AdministratorRoleName))
                 {
@@ -36,7 +41,34 @@
             }
         }
 
-        public static IApplicationBuilder AddDefaultSecurityHeaders(this IApplicationBuilder app, SecurityHeadersBuilder builder)
+        private static async Task SeedUser(UserManager<BankUser> userManager, BankSystemDbContext dbContext)
+        {
+            if (!dbContext.Users.Any())
+            {
+                var user = new BankUser
+                {
+                    Email = "test@test.com",
+                    FullName = "Test Testov",
+                    UserName = "test@test.com",
+                    EmailConfirmed = true,
+                    BankAccounts = new List<BankAccount>
+                    {
+                        new BankAccount
+                        {
+                            Balance = 10000,
+                            CreatedOn = DateTime.UtcNow,
+                            Name = "Main account",
+                            UniqueId = "ABCJ98131785"
+                        }
+                    }
+                };
+
+                await userManager.CreateAsync(user, "test123");
+            }
+        }
+
+        public static IApplicationBuilder AddDefaultSecurityHeaders(this IApplicationBuilder app,
+            SecurityHeadersBuilder builder)
             => app.UseMiddleware<SecurityHeadersMiddleware>(builder.Policy());
     }
 }
