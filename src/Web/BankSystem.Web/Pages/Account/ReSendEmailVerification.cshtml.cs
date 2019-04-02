7d3abbe7da5bb4503b0741648dfbccfeafc48a0f
Melik Pehlivanov <melikpehlivanov@gmail.com>
2019-04-02 22:06:46 +0300

    Add ReSendEmailVerification

---
+++ src/Web/BankSystem.Web/Pages/Account/ReSendEmailVerification.cshtml.cs
@@ -0,0 +1,77 @@
+﻿namespace BankSystem.Web.Pages.Account
+{
+    using BankSystem.Models;
+    using Common;
+    using Common.EmailSender.Interface;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using Models;
+    using System.ComponentModel.DataAnnotations;
+    using System.Text.Encodings.Web;
+    using System.Threading.Tasks;
+
+    [AllowAnonymous]
+    public class ReSendEmailVerification : BasePageModel
+    {
+        private readonly UserManager<BankUser> userManager;
+        private readonly SignInManager<BankUser> signInManager;
+        private readonly IEmailSender emailSender;
+
+        public ReSendEmailVerification(
+            UserManager<BankUser> userManager,
+            SignInManager<BankUser> signInManager,
+            IEmailSender emailSender)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+            this.emailSender = emailSender;
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        [BindProperty]
+        public RegisterModel.ReCaptchaModel Recaptcha { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.Page();
+            }
+
+            var user = await this.signInManager.UserManager.FindByNameAsync(this.Input.Email);
+            if (user == null)
+            {
+                this.ShowErrorMessage(NotificationMessages.TryAgainLaterError);
+                return this.Page();
+            }
+
+            var code = await this.userManager.GenerateEmailConfirmationTokenAsync(user);
+            var callbackUrl = this.Url.Page(
+                EmailConfirmationPage,
+                null,
+                new { userId = user.Id, code },
+                this.Request.Scheme);
+            await this.emailSender.SendEmailAsync(GlobalConstants.BankSystemEmail, this.Input.Email,
+                EmailSubject,
+                string.Format(EmailMessage, HtmlEncoder.Default.Encode(callbackUrl)));
+
+            this.ShowSuccessMessage(NotificationMessages.EmailVerificationLinkSendSuccessfully);
+            return this.RedirectToHome();
+        }
+
+        public class InputModel
+        {
+            [Required]
+            [EmailAddress]
+            [Display(Name = "Email")]
+            public string Email { get; set; }
+        }
+
+        public class ReCaptchaModel : BaseReCaptchaModel
+        {
+        }
+    }
+}
