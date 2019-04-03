6637b948f21fc9cd871d707fb35ec34ffa35bc08
Melik Pehlivanov <melikpehlivanov@gmail.com>
2019-04-03 13:31:38 +0300

    Add unit tests for GetBankAsync method in BankService

---
+++ src/Tests/CentralApi.Services.Tests/Tests/BankServiceTests.cs
@@ -0,0 +1,118 @@
+﻿namespace CentralApi.Services.Tests.Tests
+{
+    using CentralApi.Models;
+    using Data;
+    using FluentAssertions;
+    using Implementations;
+    using Interfaces;
+    using Microsoft.EntityFrameworkCore;
+    using Models.Banks;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Xunit;
+
+    public class BankServiceTests : BaseTest
+    {
+        private const string SampleBankName = "Bank system";
+        private const string SampleBankCountry = "Bulgaria";
+        private const string SampleBankSwiftCode = "ABC";
+        private const string SamplePaymentUrl = "https://localhost:56013/pay";
+        private const string SampleIdentificationNumbers = "10";
+
+        private readonly CentralApiDbContext dbContext;
+        private readonly IBanksService banksService;
+
+        public BankServiceTests()
+        {
+            this.dbContext = base.DatabaseInstance;
+            this.banksService = new BanksService(this.dbContext);
+        }
+
+        [Fact]
+        public async Task GetBankAsync_WithInvalidBankName_ShouldReturnNull()
+        {
+            // Arrange
+            await this.SeedBanks(10);
+
+            // Act
+            var result = await this.banksService
+                .GetBankAsync<BankServiceModel>(null, SampleBankCountry, SampleBankSwiftCode);
+
+            // Assert
+            result
+                .Should()
+                .BeNull();
+        }
+
+        [Fact]
+        public async Task GetBankAsync_WithInvalidBankCountry_ShouldReturnNull()
+        {
+            // Arrange
+            await this.SeedBanks(5);
+
+            // Act
+            var result = await this.banksService
+                .GetBankAsync<BankServiceModel>(SampleBankName, null, SampleBankSwiftCode);
+
+            // Assert
+            result
+                .Should()
+                .BeNull();
+        }
+
+        [Fact]
+        public async Task GetBankAsync_WithInvalidBankSwiftCode_ShouldReturnNull()
+        {
+            // Arrange
+            await this.SeedBanks(3);
+
+            // Act
+            var result = await this.banksService
+                .GetBankAsync<BankServiceModel>(SampleBankName, SampleBankCountry, null);
+
+            // Assert
+            result
+                .Should()
+                .BeNull();
+        }
+
+        [Fact]
+        public async Task GetBankAsync_WithValidArguments_ShouldReturnCorrectEntity()
+        {
+            // Arrange
+            await this.SeedBanks(3);
+            var expectedBank = await this.dbContext.Banks.FirstOrDefaultAsync();
+
+            // Act
+            var result = await this.banksService
+                .GetBankAsync<BankListingServiceModel>(expectedBank.Name, expectedBank.SwiftCode, expectedBank.Location);
+
+            // Assert
+            result
+                .Should()
+                .Match(b => b.As<BankListingServiceModel>().Id == expectedBank.Id);
+        }
+
+        private async Task SeedBanks(int count)
+        {
+            var banks = new List<Bank>();
+            for (int i = 1; i <= count; i++)
+            {
+                var bank = new Bank
+                {
+                    Id = i.ToString(),
+                    Name = $"{SampleBankName}_{i}",
+                    Location = $"{SampleBankCountry}_{i}",
+                    SwiftCode = $"{SampleBankSwiftCode}_{i}",
+                    PaymentUrl = SamplePaymentUrl,
+                    BankIdentificationCardNumbers = $"{SampleIdentificationNumbers}{i}",
+                };
+
+                banks.Add(bank);
+            }
+
+            await this.dbContext.Banks.AddRangeAsync(banks);
+            await this.dbContext.SaveChangesAsync();
+        }
+    }
+}
