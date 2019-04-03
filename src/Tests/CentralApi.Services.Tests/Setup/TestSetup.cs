48379c9284700f81f3fdd04ab6fa29a791d1e8c4
Melik Pehlivanov <melikpehlivanov@gmail.com>
2019-04-03 13:25:05 +0300

    Add test setup

---
+++ src/Tests/CentralApi.Services.Tests/Setup/TestSetup.cs
@@ -0,0 +1,27 @@
+﻿namespace CentralApi.Services.Tests.Setup
+{
+    using AutoMapper;
+    using BankSystem.Common.AutoMapping.Profiles;
+
+    public class TestSetup
+    {
+        private static readonly object Sync = new object();
+        private static bool mapperInitialized;
+
+        public static void InitializeMapper()
+        {
+            lock (Sync)
+            {
+                if (!mapperInitialized)
+                {
+                    Mapper.Initialize(config =>
+                    {
+                        config.AddProfile<DefaultProfile>();
+                    });
+
+                    mapperInitialized = true;
+                }
+            }
+        }
+    }
+}
