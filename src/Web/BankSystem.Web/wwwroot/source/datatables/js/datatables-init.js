6299b74879ad4bc383ac8218381a216e8b8d91eb
Martin Mladenov <30376060+martinmladenov@users.noreply.github.com>
2019-04-08 16:32:41 +0300

    Move DataTables to vendor bundle

--- src/Web/BankSystem.Web/wwwroot/source/datatables/js/datatables-init.js
+++
@@ -1,7 +0,0 @@
-﻿// Automatically initialises DataTables with the datatable-init class
-
-$(document).ready(function () {
-    $('.datatable-init').DataTable({
-        responsive: true
-    });
-});
