30d459f98a9d425778e1146b7f0de3e5fc043ab1
Martin Mladenov <30376060+martinmladenov@users.noreply.github.com>
2019-04-10 18:32:40 +0300

    Use SignatureVerificationModel in SignatureVerificationUtil

--- src/Common/BankSystem.Common/Utils/SignatureVerificationUtil.cs
+++ src/Common/BankSystem.Common/Utils/SignatureVerificationUtil.cs
@@ -4,38 +4,43 @@
     using System.Globalization;
     using System.Security.Cryptography;
     using System.Text;
+    using Models;
 
     public static class SignatureVerificationUtil
     {
         private const int TimestampValidityBeforeIssuanceInMinutes = 2;
         private const int TimestampValidityAfterIssuanceInMinutes = 2;
 
-        public static string DecryptDataAndVerifySignature(
-            string decryptionPrivateKey,
-            string signaturePublicKey,
-            string encryptedKey,
-            string encryptedIv,
-            string data,
-            string signature)
+        public static string DecryptDataAndVerifySignature(SignatureVerificationModel model)
         {
+            if (!ValidationUtil.IsObjectValid(model))
+            {
+                return null;
+            }
+
             // Decrypt
             string decrypted;
             using (var rsa = RSA.Create())
             {
-                RsaExtensions.FromXmlString(rsa, decryptionPrivateKey);
-                var decryptedKey = rsa.Decrypt(Convert.FromBase64String(encryptedKey), RSAEncryptionPadding.Pkcs1);
-                var decryptedIv = rsa.Decrypt(Convert.FromBase64String(encryptedIv), RSAEncryptionPadding.Pkcs1);
+                RsaExtensions.FromXmlString(rsa, model.DecryptionPrivateKey);
+                var decryptedKey = rsa.Decrypt(Convert.FromBase64String(model.EncryptedKey),
+                    RSAEncryptionPadding.Pkcs1);
 
-                decrypted = CryptographyExtensions.Decrypt(Convert.FromBase64String(data), decryptedKey, decryptedIv);
+                var decryptedIv = rsa.Decrypt(Convert.FromBase64String(model.EncryptedIv),
+                    RSAEncryptionPadding.Pkcs1);
+
+                decrypted = CryptographyExtensions.Decrypt(Convert.FromBase64String(model.Data), decryptedKey,
+                    decryptedIv);
             }
 
             // Verify
             using (var rsa = RSA.Create())
             {
-                RsaExtensions.FromXmlString(rsa, signaturePublicKey);
+                RsaExtensions.FromXmlString(rsa, model.SignaturePublicKey);
 
                 var decrypt = Encoding.UTF8.GetBytes(decrypted);
-                var isVerified = rsa.VerifyData(decrypt, Convert.FromBase64String(signature), HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+                var isVerified = rsa.VerifyData(decrypt, Convert.FromBase64String(model.Signature),
+                    HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
 
                 if (!isVerified)
                 {
