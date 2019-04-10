569aa3327a9c892f9a0a5480191194cf7a62823d
Melik Pehlivanov <melikpehlivanov@gmail.com>
2019-04-10 16:56:30 +0300

    Add model for SignatureVerificationUtil

---
+++ src/Common/BankSystem.Common/Utils/Models/SignatureVerificationModel.cs
@@ -0,0 +1,25 @@
+﻿namespace BankSystem.Common.Utils.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class SignatureVerificationModel
+    {
+        [Required]
+        public string DecryptionPrivateKey { get; set; }
+
+        [Required]
+        public string SignaturePublicKey { get; set; }
+
+        [Required]
+        public string EncryptedKey { get; set; }
+
+        [Required]
+        public string EncryptedIv { get; set; }
+
+        [Required]
+        public string Data { get; set; }
+
+        [Required]
+        public string Signature { get; set; }
+    }
+}
