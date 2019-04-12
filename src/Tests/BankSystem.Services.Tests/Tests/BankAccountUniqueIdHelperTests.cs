ebbc9d5e554f95c09f75b804b43610bc7fb96af6
Melik Pehlivanov <melikpehlivanov@gmail.com>
2019-04-12 16:47:41 +0300

    Add unit tests for IsUniqueIdValid method in BankAccountUniqueIdHelper

---
+++ src/Tests/BankSystem.Services.Tests/Tests/BankAccountUniqueIdHelperTests.cs
@@ -0,0 +1,46 @@
+﻿namespace BankSystem.Services.Tests.Tests
+{
+    using FluentAssertions;
+    using Implementations;
+    using Interfaces;
+    using Xunit;
+
+    public class BankAccountUniqueIdHelperTests : BaseTest
+    {
+        private readonly IBankAccountUniqueIdHelper bankAccountUniqueIdHelper;
+
+        public BankAccountUniqueIdHelperTests()
+        {
+            this.bankAccountUniqueIdHelper = new BankAccountUniqueIdHelper(this.MockedBankConfiguration.Object);
+        }
+
+        [Theory]
+        [InlineData(" ABCJ98131783")]
+        [InlineData("ABCJ98131785 ")]
+        [InlineData("   asd  ")]
+        public void IsUniqueIdValid_WithInvalidId_ShouldReturnFalse(string id)
+        {
+            // Act
+            var result = this.bankAccountUniqueIdHelper.IsUniqueIdValid(id);
+
+            // Assert
+            result
+                .Should()
+                .BeFalse();
+        }
+
+        [Theory]
+        [InlineData("ABCY12054790")]
+        [InlineData("ABCY52630755")]
+        public void IsUniqueIdValid_WithValidId_ShouldReturnTrue(string id)
+        {
+            // Act
+            var result = this.bankAccountUniqueIdHelper.IsUniqueIdValid(id);
+
+            // Assert
+            result
+                .Should()
+                .BeTrue();
+        }
+    }
+}
