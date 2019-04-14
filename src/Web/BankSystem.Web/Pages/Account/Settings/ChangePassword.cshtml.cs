866e93efebb37bf537782f2ef401e899a234ff94
Martin Mladenov <30376060+martinmladenov@users.noreply.github.com>
2019-04-14 20:14:12 +0300

    Add password validation when changing password

--- src/Web/BankSystem.Web/Pages/Account/Settings/ChangePassword.cshtml.cs
+++ src/Web/BankSystem.Web/Pages/Account/Settings/ChangePassword.cshtml.cs
@@ -80,10 +80,11 @@
             public string OldPassword { get; set; }
 
             [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.",
-                MinimumLength = 6)]
             [DataType(DataType.Password)]
             [Display(Name = "New password")]
+            [StringLength(ModelConstants.User.PasswordMaxLength, MinimumLength = ModelConstants.User.PasswordMinLength)]
+            [RegularExpression(ModelConstants.User.PasswordRegex,
+                ErrorMessage = ModelConstants.User.PasswordErrorMessage)]
             public string NewPassword { get; set; }
 
             [DataType(DataType.Password)]
