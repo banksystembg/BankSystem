b46f06fde1fcba5e4e1e756cb961dd60e82554aa
Martin Mladenov <30376060+martinmladenov@users.noreply.github.com>
2019-04-14 16:55:41 +0300

    Remove unused ShortName column in CentralApi database

--- src/Data/CentralApi/CentralApi.Models/Bank.cs
+++ src/Data/CentralApi/CentralApi.Models/Bank.cs
@@ -7,9 +7,6 @@
         public string Id { get; set; }
 
         [Required]
-        public string ShortName { get; set; }
-
-        [Required]
         public string Name { get; set; }
 
         [Required]
