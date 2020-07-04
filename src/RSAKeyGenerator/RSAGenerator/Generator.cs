71c3228aa3d27766a487e99faca83d0af63eada5
Melik Pehlivanov <melikpehlivanov@gmail.com>
2020-07-04 11:26:32 +0300

    Implement Rsa key generator

---
+++ src/RSAKeyGenerator/RSAGenerator/Generator.cs
@@ -0,0 +1,18 @@
+﻿namespace RSAGenerator
+{
+    using System;
+    using System.Security.Cryptography;
+
+    public static class Generator
+    {
+        public static void Main()
+        {
+            var rsa = new RSACryptoServiceProvider(4096);
+            var publicKey = rsa.ToXmlString(false);
+            var privateKey = rsa.ToXmlString(true);
+            Console.WriteLine($"Public key: {publicKey}");
+            Console.WriteLine("=====================================================================");
+            Console.WriteLine($"Private key: {privateKey}");
+        }
+    }
+}
